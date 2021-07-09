372349f2fdd0bd1f6c7407b33c880f8c0460ac70
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-07-09 16:01:59 +0100

    Change the values variable to int

--- run.py
+++ run.py
@@ -22,9 +22,10 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    
+
     sales_data = data_str.split(",")
     validate_data(sales_data)
+
 
 def validate_data(values):
     """
@@ -33,12 +34,12 @@ def validate_data(values):
     or if there aren't exactly 6 values
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values needed, you provided {len(values)}"
-            )
+                f"Exactly 6 values needed, you provided {len(values)}")
     except ValueError as e:
         print(f"invalid data: {e}, please try again. \n")
-        get_sales_data()
+
 
 get_sales_data()
